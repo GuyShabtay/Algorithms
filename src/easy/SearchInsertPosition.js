bbd9163b20a0d545d87a45658be268137a25d0de
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-02-25 23:43:41 +0200

    add Search Insert Position

---
+++ src/easy/SearchInsertPosition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let i;
+  for(i=0;i<nums.length;i++)
+  {
+      if (nums[i]===target)
+      return i;
+  }
+  for(i=0;i<nums.length;i++)
+  {
+      if (nums[i]>target)
+      return i;
+  }
+  return i ;
+};
